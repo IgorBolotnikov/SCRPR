678f395a8ecb20e3175677b0aedf704a55dbca7d
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-01-14 18:39:23 +0200

    Fix static files bug

--- project/settings.py
+++ project/settings.py
@@ -164,7 +164,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Miscellaneous
