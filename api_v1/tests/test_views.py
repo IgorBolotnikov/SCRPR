34f789d7b92a08fcfe3a2f210a212e89ecab396a
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-08 14:46:57 +0200

    Cleanup code

--- api_v1/tests/test_views.py
+++ api_v1/tests/test_views.py
@@ -71,22 +71,6 @@ class TestNewsView(PaginationMixin):
     objects_num = 40
     viewset = False
 
-    # def test_get_response(self):
-    #     request = APIRequestFactory().get('/')
-    #     news = mixer.cycle(40).blend('scrpr.NewsPost')
-    #     response = views.NewsListView.as_view()(request)
-    #     pagination = response.data.get('pagination')
-    #     results = response.data.get('results')
-    #     assert response.status_code == 200, 'Should be accessible by anyone'
-    #     assert results, 'Should return News results'
-    #     assert len(results) == 10, 'Should return last 10 News'
-    #     assert pagination, 'Pagination should be in response data'
-    #     assert pagination['page'] == 1, 'Should return first page by default'
-    #     assert pagination['prev_page'] == None, 'Should not have previous page'
-    #     assert pagination['next_page'] == 2, 'Should have next page'
-    #     assert pagination['last_page'] == 4, 'Should have correct last page'
-    #     assert pagination['page_size'] == 10, 'Page size should be 10'
-
 
 class TestFavoritesGamesListView(PaginationMixin):
     model_class = 'scrpr.FavoriteGameQuery'
@@ -136,21 +120,3 @@ class TestFavoritesJobsListView(PaginationMixin):
         response = self.view_class.as_view(self.viewset_method)(request)
         assert response.status_code == 200, \
             'Should be accessible only by authenticated users'
-
-
-class TestUserView:
-    def test_get_response_for_anonymous_user(self):
-        request = APIRequestFactory().get('/')
-        request.user = AnonymousUser()
-        response = UserView.as_view()(request)
-        assert response.status_code == 401, \
-            'Should not be accessible by anonymous users'
-
-    def test_get_response_for_authenticated_user(self):
-        request = APIRequestFactory().get('/')
-        user = mixer.blend('authentication.User')
-        force_authenticate(request, user=user)
-        response = UserView.as_view()(request)
-        assert response.status_code == 200, \
-            'Should be accessible only by authenticated users'
-        assert user.id = response.data['id'], 'Should return current user info'
