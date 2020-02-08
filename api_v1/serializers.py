a7f8ce7c055cc94c8270231dfb04e1c769d4e7df
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-08 12:22:11 +0200

    Add Game favorites viewset

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
 
-from scrpr.models import NewsPost
+from scrpr.models import NewsPost, FavoriteGameQuery
 
 
 class NewsPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsPost
         fields = ('title', 'body', 'datetime_posted')
+
+
+class FavoriteGameQuerySerializer(serializers.ModelSerializer):
+    notification_frequency = serializers.CharField(
+        source='get_notification_freq_display'
+    )
+
+    class Meta:
+        model = FavoriteGameQuery
+        fields = (
+            'title',
+            'price_min',
+            'price_max',
+            'psplus_price',
+            'initial_price',
+            'free',
+            'notification_frequency',
+            'account_id'
+        )
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-from api_v1.serializers import NewsPostSerializer
-from scrpr.models import NewsPost
+from api_v1.serializers import NewsPostSerializer, FavoriteGameQuerySerializer
+from scrpr.models import NewsPost, FavoriteGameQuery
+from api_v1.permissions import IsCreator
 
 
 class NewsListView(ListAPIView):
@@ -10,3 +12,12 @@ class NewsListView(ListAPIView):
     serializer_class = NewsPostSerializer
     permission_classes = [AllowAny]
     paginate_by = 10
+
+
+class FavoritesGamesListView(ModelViewSet):
+    serializer_class = FavoriteGameQuerySerializer
+    permission_classes = [IsAuthenticated, IsCreator]
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        return FavoriteGameQuery.objects.filter(account=self.request.user)
